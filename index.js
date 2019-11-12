da0b50e37a5d916738e5a0e2ded3d93789fd9afd
Andrew Polhill <andrew.polhill@landg.com>
2019-11-12 11:45:53 +0000

    Performance improvements. - combine draw and update - remove as many function calls as possible - use basic for loops
    These changes convert the fps from 15 to around 50
    The additional speed should provide the ability to do more

--- index.js
+++ index.js
@@ -1,77 +1,52 @@
-const width = 700;
-const diameter = 3;
-const seed = 0.3;
+const width = 600;
+const diameter = 2;
 
 const canvas = document.getElementById('game');
 canvas.width = width;
 canvas.height = width;
 const ctx = canvas.getContext('2d');
 
-
-function get(cells, path) {
-  let result = 0;
-  try {
-    result = cells[path[0]][path[1]]
-  } catch(e) {
-    result = 0
-  }
-  if(!result) result = 0;
-  return result;
-}
-
 function update(cells) {
-  return cells.map((row, x) => {
-    return row.map((cell, y) => {
-      let result = [
-        get(cells, [x - 1, y - 1]),
-        get(cells, [x - 1, y]),
-        get(cells, [x - 1, y + 1]),
-        get(cells, [x, y - 1]),
-        get(cells, [x, y + 1]),
-        get(cells, [x + 1, y - 1]),
-        get(cells, [x + 1, y]),
-        get(cells, [x + 1, y + 1]),
-      ];
-      
-      result = result.reduce((a, b) => a + b, 0);
-
-      if(cell === 1) {
-        if (result < 2 || result > 3) {
-          return 0
-        }
-        return 1;
-      } else {
-        if(result === 3) {
-          return 1;
-        }
-      }
-    });
-  });
-}
-
-function draw() {
   ctx.clearRect(0, 0, width, width);
-  cells
-    .forEach((row, x) => row.forEach((cell, y) => {
-      if (cell) {
+
+  const next_cells = [];
+
+  for(let x = 0; x < cells.length; x++) { 
+    let row = cells[x];
+    for(let y = 0; y < row.length; y++) {
+      const cell = cells[x][y];
+      let count =
+      ((cells[x - 1] && cells[x - 1][y - 1]) ? cells[x - 1][y - 1] : 0) +
+      ((cells[x - 1] && cells[x - 1][y]) ? cells[x - 1][y] : 0) +
+      ((cells[x - 1] && cells[x - 1][y + 1]) ? cells[x - 1][y + 1] : 0) +
+      ((cells[x] && cells[x][y - 1]) ? cells[x][y - 1] : 0) +
+      ((cells[x] && cells[x][y + 1]) ? cells[x][y + 1] : 0) +
+      ((cells[x + 1] && cells[x + 1][y - 1]) ? cells[x + 1][y - 1] : 0) +
+      ((cells[x + 1] && cells[x + 1][y]) ? cells[x + 1][y] : 0) +
+      ((cells[x + 1] && cells[x + 1][y + 1]) ? cells[x + 1][y + 1] : 0);
+
+      if(!next_cells[x]) next_cells[x] = [];
+
+      if((cell === 1 && (count === 3 || count === 2)) || (cell === 0 && count === 3)) {
         ctx.fillStyle = '#1cb500';
-        ctx.fillRect(x * diameter, y * diameter, diameter, diameter);
+        ctx.fillRect(x * diameter, y * diameter, diameter, diameter);  
+        next_cells[x][y] = 1;
+      } else {
+        next_cells[x][y] = 0;
       }
-    }));  
+    }
+  }
+
+  return next_cells;
 }
 
-function main() {
-
-
-  draw(cells);
-  cells = update(cells);
-  requestAnimationFrame(() => main(cells));
+function main(cells) {  
+  const next = update(cells);
+  requestAnimationFrame(() => main(next));
 }
-
-let cells = Array.from({ length: width / diameter })
-.map(() => Array.from({ length: width / diameter })
-.map(() => Math.round(Math.random() - seed)));
-
 
 // Start things off
-requestAnimationFrame(() => main(cells));
+requestAnimationFrame(() => main(Array
+  .from({ length: width / diameter })
+  .map(() => Array.from({ length: width / diameter })
+  .map(() => Math.round(Math.random())))));
