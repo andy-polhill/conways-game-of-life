0d7d18651c48486e5a1ac08c83ea3779b401c77a
thatguynamedandy <apolhill@gmail.com>
2019-11-12 17:43:31 +0000

    Use an off screen canvas for the plotting of cells

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const width = 600;
-const diameter = 2;
+const diameter = 4;
 
 const canvas = document.getElementById('game');
 canvas.width = width;
 canvas.height = width;
 const ctx = canvas.getContext('2d');
 
+const off_screen = document.createElement('canvas');
+off_screen.width = width;
+off_screen.height = width;
+const plotter = canvas.getContext('2d');
+
 function update(cells) {
-  ctx.clearRect(0, 0, width, width);
+  plotter.clearRect(0, 0, width, width);
 
   const next_cells = [];
 
@@ -28,8 +33,8 @@ function update(cells) {
       if(!next_cells[x]) next_cells[x] = [];
 
       if((cell === 1 && (count === 3 || count === 2)) || (cell === 0 && count === 3)) {
-        ctx.fillStyle = '#1cb500';
-        ctx.fillRect(x * diameter, y * diameter, diameter, diameter);  
+        plotter.fillStyle = '#1cb500';
+        plotter.fillRect(x * diameter, y * diameter, diameter, diameter);  
         next_cells[x][y] = 1;
       } else {
         next_cells[x][y] = 0;
@@ -37,6 +42,7 @@ function update(cells) {
     }
   }
 
+  ctx.drawImage(off_screen, 0, 0);
   return next_cells;
 }
 
